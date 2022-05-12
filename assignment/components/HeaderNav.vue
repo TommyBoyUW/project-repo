2e678f678697666d12d65b794e904e8d3fdefa7d
TommyBoyUW <sepant@uw.edu>
2022-05-11 22:02:03 -0400

    uploading assignment 9

---
+++ assignment/components/HeaderNav.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <nav>
+    <b-navbar class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center" style="background-color: #cde8e3">
+    <b-navbar-nav class="bars" >
+    <b-nav-item class="bar" to="/">Home</b-nav-item>
+    <b-nav-item class="bar" to="/page2">Speech </b-nav-item>
+    <b-nav-item class="bar" to="/page3">Language</b-nav-item>
+    <b-nav-item class="bar" to="/page4">Literacy</b-nav-item>
+    <b-nav-item class="bar" href="https://sarahctheslp.com">Store</b-nav-item>
+    </b-navbar-nav>
+    </b-navbar>
+    <img src="../static/1.png" class="img-fluid mx-auto d-block" width="100%">
+    </nav>
+  </div>
+</template>
+
+
+<script>
+</script>
+
+
+<style scoped lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.navbar.navbar-expand-lg.navbar-light.bg-light{
+background-color: #cde8e3!important;
+font-size: 1.5rem;
+padding: 2.5% 0%;
+}
+
+.bar{
+padding: 0 0%;
+}
+
+.bars {
+padding-left:auto;
+  padding-right:auto;
+}
+
+nav {
+padding:0 0 2.5% 0;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+}}}
+</style>
---
+++ assignment/pages/lessonProfile.vue
@@ -0,0 +1,27 @@
+<template>
+        <div class="card text-center col-12 col-lg-3" >
+          <img :src="lesson.picture" :alt="lesson.alt" id="border" class="frame card-img-top img-fluid rounded">
+          <h4 class="text"> {{ lesson.title }} </h4>
+          <div class="description"> {{ lesson.description }} </div>
+        </div>
+
+</template>
+
+
+
+<script>
+export default {
+    name: 'lessonProfile',
+    props: {
+        lesson: Object,
+            default: function () {
+                return { reactive: true}
+}}}
+</script>
+
+
+<style scoped>
+.card {
+  margin: 2.5%;
+}
+</style>
